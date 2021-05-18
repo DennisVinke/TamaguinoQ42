8c247f193c1f094adf84c42cd288d6500eb08ce8
Remco Veldkamp <remcoder@gmail.com>
2021-05-18 16:51:00 +0200

    Add light sensor usage

--- Tamaquino/Tamaquino.ino
+++ Tamaquino/Tamaquino.ino
@@ -16,6 +16,8 @@
 const int button1Pin = 9;
 const int button2Pin = 8;
 const int button3Pin = 7;
+const int lightSensorPin = A0;
+
 const unsigned long dayNightCycleSeconds = 240;
 const int dayStartTime = 800;
 // Calculate the millis offset so our program always starts at dawn instead of
@@ -36,10 +38,13 @@ bool sleeping = false;
 float poopometer = 0;
 int poopCoordinates[3] = {0, 0, 0};
 
+int lightValue = 0;
+
 void setup() {
   pinMode(button1Pin, INPUT_PULLUP);
   pinMode(button2Pin, INPUT_PULLUP);
   pinMode(button3Pin, INPUT_PULLUP);
+  
 
   pinMode(13, OUTPUT);
 
@@ -52,6 +57,15 @@ void setup() {
 }
 
 void loop() {
+  lightValue = analogRead(lightSensorPin);  // read the input pin
+  // Serial.print("light ");
+  // Serial.println(lightValue);
+  if (lightValue > 600) {
+    happiness--;
+    if (happiness < 0) {
+      happiness = 0;
+    }
+  }
 
   if (dead) {
     drawDead();
